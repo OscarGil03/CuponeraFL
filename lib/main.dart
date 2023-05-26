29caff41e3466e6b492482063ca147418e8dd600
GonBM <gbarrantesinf@gmail.com>
2023-05-25 20:20:54 -0700

    Prueba de inicializacion de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
+import 'package:cuponera_fl/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cuponera_fl/routes/routes.dart';
 import 'package:cuponera_fl/themes/themes.dart';
 
-
-void main() {
+void main() async {
+  //Se asegura de tener una instancia de WidgetsFlutterBinding
+  //Lo cual permite usar los canales de plataformas
+  WidgetsFlutterBinding.ensureInitialized();
+  //Inicializa Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   /* Estas lineas utilizan el paquete Services de flutter
   para impedir que se gire el dispositivo
   */
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
-    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]
-  );
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
